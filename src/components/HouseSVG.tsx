16a1a95c8548ff7f9084bffb72d1c012303439a5
zeyneb2626 <zeyneb.madi2626@gmail.com>
2025-05-29 08:57:12 +0400

    Update house SVG to match reference exactly

--- src/components/HouseSVG.tsx
+++ src/components/HouseSVG.tsx
@@ -15,68 +15,20 @@ const HouseSVG: React.FC<HouseSVGProps> = ({ rating }) => {
       xmlns="http://www.w3.org/2000/svg"
       className="w-full h-full"
     >
-      {/* House outline */}
+      {/* House outline in L shape */}
       <path
-        d="M40,100 
+        d="M60,180 
+           L60,120 
+           L100,80 
            L100,40 
-           L160,100
-           L160,180 
-           L40,180 
-           L40,100 
-           Z"
+           L140,40 
+           L140,80"
         fill="none"
         className="energy-rating-transition"
         stroke={color}
-        strokeWidth="4"
-      />
-
-      {/* Chimney */}
-      <path
-        d="M130,60 
-           L130,40 
-           L145,40 
-           L145,70"
-        fill="none"
-        className="energy-rating-transition"
-        stroke={color}
-        strokeWidth="4"
-      />
-
-      {/* Door frame */}
-      <path
-        d="M85,180 
-           L85,130 
-           L115,130 
-           L115,180"
-        fill="none"
-        className="energy-rating-transition"
-        stroke={color}
-        strokeWidth="4"
-      />
-
-      {/* Windows */}
-      <path
-        d="M60,120 
-           L60,140 
-           L80,140 
-           L80,120 
-           L60,120"
-        fill="none"
-        className="energy-rating-transition"
-        stroke={color}
-        strokeWidth="4"
-      />
-
-      <path
-        d="M120,120 
-           L120,140 
-           L140,140 
-           L140,120 
-           L120,120"
-        fill="none"
-        className="energy-rating-transition"
-        stroke={color}
-        strokeWidth="4"
+        strokeWidth="6"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
